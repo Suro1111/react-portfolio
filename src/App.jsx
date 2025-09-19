2aa22a3b3094581b3f408bd214e6282f6c6c9da4
Suro1111 <SurH49@gmail.com>
2025-09-19 16:25:23 +0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { Header } from "./components/Header/Header";
 import { Main } from "./components/Main/Main";
 import { Footer } from "./components/Footer/Footer";
 import { Top } from "./components/Top/Top";
+import { Theme } from "./components/Theme/Theme";
 
 //* css
 import "./assets/global/global.css";
-import { Theme } from "./components/Theme/Theme";
 
 export const App = () => {
   return (
